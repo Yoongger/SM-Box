029ec16f37a9dabec0b841416f5129843df367f1
ZhangLingge <50951231+Yoongger@users.noreply.github.com>
2025-02-06 19:38:10 +0800

    Update entrypoint.sh

--- config/entrypoint.sh
+++ config/entrypoint.sh
@@ -1,13 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 
 # 检查配置文件
-check_config() {
-    if ! /opt/sing-box/sing-box check -c /opt/sing-box/config.json; then
-        echo "❌ Singbox 配置检查失败"
-        exit 1
-    fi
-    echo "✅ Singbox 配置检查通过"
-}
-
-check_config
+if ! /opt/sing-box/sing-box check -c /opt/sing-box/config.json; then
+    echo "❌ Singbox 配置检查失败"
+    exit 1
+fi
+echo "✅ Singbox 配置检查通过"
 exec /usr/bin/supervisord -c /etc/supervisord.conf
